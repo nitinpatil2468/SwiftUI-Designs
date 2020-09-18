f8dd4b01e8576bb7460a7f88d784f1a723e70806
nitinpatil2468 <30385369+nitinpatil2468@users.noreply.github.com>
2020-09-18 13:04:34 +0530

    Add files via upload
    initial commit

---
+++ SwiftUIProject/SwiftUIProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIProject
+//
+//  Created by Nitin Patil on 14/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Mumbai!")
+            .font(.title).foregroundColor(.red)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
